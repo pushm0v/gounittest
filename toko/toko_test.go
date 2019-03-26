1ddd29fb9637bb96f9e362ee4b73cb9eccfd9c7a
Bherly <bherly.novrandy@gmail.com>
2019-03-26 16:45:30 +0700

    Add unit test for toko

--- toko/toko_test.go
+++ toko/toko_test.go
@@ -2,7 +2,46 @@ package toko
 
 import (
 	"testing"
+
+	"gotest.tools/assert"
 )
 
-func TestTokoBuka(t *testing.T) {
+func TestWhenTokoBuka(t *testing.T) {
+	toko := NewToko()
+	toko.Buka()
+
+	assert.Equal(t, true, toko.IsBuka())
+}
+
+func TestWhenTokoTutup(t *testing.T) {
+	toko := NewToko()
+	toko.Tutup()
+
+	assert.Equal(t, false, toko.IsBuka())
+}
+
+func TestTambahStokMinuman(t *testing.T) {
+	toko := NewToko()
+	toko.Buka()
+	m := Minuman{
+		Nama:  "Sprite",
+		Harga: 5000,
+	}
+	toko.TambahStokMinuman(m, 10)
+	result := toko.JualMinuman(m, 10)
+
+	assert.Equal(t, true, result)
+}
+
+func TestJualMinumanWhenNotAvailable(t *testing.T) {
+	toko := NewToko()
+	toko.Buka()
+	m := Minuman{
+		Nama:  "Sprite",
+		Harga: 5000,
+	}
+	toko.TambahStokMinuman(m, 1)
+	result := toko.JualMinuman(m, 10)
+
+	assert.Equal(t, false, result)
 }
